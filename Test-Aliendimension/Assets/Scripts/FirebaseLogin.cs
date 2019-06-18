f9683a0979bb6aa83e6ab6eb0efcdead2414b292
atefboutara <atef.boutara.98@gmail.com>
2019-06-18 22:12:02 +0100

    Game Uploaded

---
+++ Test-Aliendimension/Assets/Scripts/FirebaseLogin.cs
@@ -0,0 +1,103 @@
+﻿using Firebase.Auth;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FirebaseLogin : MonoBehaviour
+{
+
+    #region Variables
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    public InputField Email, Mdp;
+
+    #endregion
+
+    #region Monobehaviour
+
+    void Start()
+    {
+        InitializeFirebase();
+    }
+
+    #endregion
+
+    #region Firebase
+
+    void InitializeFirebase()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+        auth.SignOut();
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                Debug.Log("Signed in " + user.UserId);
+                SceneManager.LoadScene("SceneAR");
+            }
+        }
+    }
+
+    #endregion
+
+    #region Buttons Functions
+
+    public void ButtonConnexion()
+    {
+        auth.SignInWithEmailAndPasswordAsync(Email.text, Mdp.text).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.Log("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.Log("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    public void BoutonInscription()
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(Email.text, Mdp.text).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.Log("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.Log("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    #endregion
+
+}
---
+++ Test-Aliendimension/Assets/Scripts/FirebaseStocked3D.cs
@@ -0,0 +1,99 @@
+﻿using Firebase.Storage;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine.Animations;
+using UnityEngine;
+using UnityEngine.Networking;
+using Vuforia;
+
+public class FirebaseStocked3D : MonoBehaviour
+{
+    #region Variables
+
+    FirebaseStorage storage;
+    StorageReference storage_ref;
+    AssetBundle bundle;
+
+    public RuntimeAnimatorController AnimationController;
+
+    #region small fix
+
+    GameObject Loaded3DLogo = null;
+
+    #endregion
+
+    #endregion
+
+    #region Monobehaviour
+
+    void Start()
+    {
+        StartCoroutine(LoadLogo());
+    }
+
+    void Update()
+    {
+        if(Loaded3DLogo != null)//This is a small fix because the loaded model from firebase was still showing (i tried on the editor) even when there was no marker
+        {
+            if (isTrackingMarker())
+            {
+                if (!Loaded3DLogo.activeInHierarchy)
+                    Loaded3DLogo.SetActive(true);
+            }
+            else
+                Loaded3DLogo.SetActive(false);
+        }
+    }
+
+    #endregion
+
+    #region Firebase
+
+    IEnumerator LoadLogo()
+    {
+        using (UnityWebRequest uwr = UnityWebRequestAssetBundle.GetAssetBundle("https://firebasestorage.googleapis.com/v0/b/testunity-496ff.appspot.com/o/Models%2Fpepsilogo.unity3d?alt=media&token=547f9885-352a-4852-8b81-77c50593e135"))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                Debug.Log("Downloaded object");
+                bundle = DownloadHandlerAssetBundle.GetContent(uwr);
+
+                yield return bundle;
+                            
+                var loadAsset = bundle.LoadAssetAsync<GameObject>("pepsi.FBX");
+                yield return loadAsset;
+                
+                GameObject Logo = (GameObject) Instantiate(loadAsset.asset);
+                Logo.transform.rotation = new Quaternion(0, 0, 0, 0);
+                Logo.transform.position = new Vector3(-0.017f, 0.07900001f, 0.009000001f);
+                Logo.AddComponent<Animator>().runtimeAnimatorController = AnimationController;
+                Logo.transform.parent = transform;
+
+                Loaded3DLogo = Logo;
+            }
+        }
+
+
+    }
+
+    #endregion
+
+    #region Functions
+
+    private bool isTrackingMarker()
+    {
+        var trackable = gameObject.GetComponent<TrackableBehaviour>();
+        var status = trackable.CurrentStatus;
+        return status == TrackableBehaviour.Status.TRACKED;
+    }
+
+    #endregion
+}
